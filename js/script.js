b8ffed9fa01cb40a60a7558961c5f19d0983c122
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-28 14:33:25 +0200

    milestone 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log('JS OK');
+// #  Fase di Preparazione
+//creo l'array di oggetti
+const ourTeam = [
+    //creo gli oggetti...
+    {
+        //...contenenti le infomrazioni delle singole persone
+        nome: 'Wayne Barnett',
+        profession: 'Founder & CEO',
+        profilePic: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        profession: 'Chief Editor',
+        profilePic: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        profession: 'Office Manager',
+        profilePic: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        profession: 'Social Media Manager',
+        profilePic: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott Estrada',
+        profession: 'Developer',
+        profilePic: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        profession: 'Graphic Designer',
+        profilePic: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
