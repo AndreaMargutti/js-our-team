0c868f6f66d8b1803cddcd827fafbadba1ced90b
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-28 14:41:26 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -33,4 +33,14 @@ const ourTeam = [
         profession: 'Graphic Designer',
         profilePic: 'barbara-ramos-graphic-designer.jpg'
     }
-]
+];
+
+//Per ogni elemento dell'array...
+for (let i = 0; i < ourTeam.length; i++) {
+    //...recupero il singolo oggetto
+    const person = ourTeam[i];
+    //Stampo in console i campi degli oggetti
+    console.log(`Full-Name: `, person.nome); //loggo il nome e cognome della persona
+    console.log('Profession: ', person.profession); //loggo la professione della persona
+    console.log('Profile Picture: ', person.profilePic); //stampo la foto come stringa
+}
